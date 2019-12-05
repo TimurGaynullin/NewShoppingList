b5dc057155a80c701d2b07d28471286fb5ae4d26
Timur <timur.gaynullin@akbarsdigital.ru>
2019-12-05 12:59:31 +0300

    Initial

---
+++ ShoppingListControllers/ShoppingListController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingList.DataBase;
+using ShoppingList.Domain;
+using ShoppingList.Domain.Abstractions;
+using ShoppingList.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShoppingList.Controllers
+{
+	//[Authorize]
+	[Route("api/[controller]")]
+	public class ShoppingListController : Controller
+	{
+		IShoppingService shoppingService;
+		DataContext db;
+		public ShoppingListController(DataContext context, IShoppingService shoppingService)
+		{
+			this.db = context;
+			this.shoppingService = shoppingService;
+			shoppingService.InitializeDatabase();
+		}
+		
+		[HttpGet]
+		public IEnumerable<Purchase> Get()
+		{
+			return shoppingService.GetPurchases();
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult Get(int id)
+		{
+			var list = shoppingService.GetPurchaseById(id);
+			if (list == null)
+				return NotFound();
+			return new ObjectResult(list);
+		}
+
+		[HttpPost]
+		public IActionResult Post([FromBody]Purchase list)
+		{
+			if (list == null)
+			{
+				return BadRequest();
+			}
+			shoppingService.PostPurchase(list);
+			return Ok(list);
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Put([FromBody]Purchase list)
+		{
+			if (list == null)
+			{
+				return BadRequest();
+			}
+			if (!db.Purchases.Any(x => x.Id == list.Id))
+			{
+				return NotFound();
+			}
+			shoppingService.PutPurchase(list);
+			return Ok(list);
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			Purchase purchase = shoppingService.FindPurchase(id);
+			if (purchase == null)
+			{
+				return NotFound();
+			}
+			shoppingService.DeletePurchaseById(purchase, id);
+			return Ok(purchase);
+		}
+	}
+}
---
+++ ShoppingListControllers/TimeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingList.DataBase;
+using ShoppingList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingList.Controllers
+{
+	[Route("api/[controller]")]
+	public class TimeController : Controller
+	{
+		DataContext db;
+		public TimeController(DataContext context)
+		{
+			this.db = context;
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult Get(DateTime dateTimeStart, DateTime dateTimeFinish)
+		{
+			IEnumerable<Purchase> list = db.Purchases.ToList<Purchase>();
+			list = list.Where(x => x.TimeOfNotation.Date >= dateTimeStart.Date && x.TimeOfNotation.Date <= dateTimeFinish.Date);
+			if (list == null)
+				return NotFound();
+			return new ObjectResult(list);
+		}
+	}
+}
